ab11d7e50cecf39a219a5b2692b9cb8efaffd77b
CodeFuller <CodeFuller@gmail.com>
2017-11-18 10:54:05 +0300

    Add sample Type Converter

---
+++ WebApiExtensibilityDemo/ModelBinding/OrderId.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace WebApiExtensibilityDemo.ModelBinding
+{
+	[TypeConverter(typeof(OrderIdTypeConverter))]
+	public class OrderId
+	{
+		public string DepartmentId { get; }
+
+		public int OrderNumber { get; }
+
+		public OrderId(string departmentId, int orderNumber)
+		{
+			DepartmentId = departmentId;
+			OrderNumber = orderNumber;
+		}
+	}
+}
---
+++ WebApiExtensibilityDemo/ModelBinding/OrderIdTypeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace WebApiExtensibilityDemo.ModelBinding
+{
+	public class OrderIdTypeConverter : TypeConverter
+	{
+		private static readonly Regex OrderIdRegex = new Regex("^(.+):(\\d+)$", RegexOptions.Compiled);
+
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+		}
+
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			var str = value as string;
+			if (str != null)
+			{
+				int orderId;
+				var match = OrderIdRegex.Match(str);
+				if (match.Success && Int32.TryParse(match.Groups[2].Value, out orderId))
+				{
+					return new OrderId(match.Groups[1].Value, orderId);
+				}
+			}
+
+			return base.ConvertFrom(context, culture, value);
+		}
+	}
+}
